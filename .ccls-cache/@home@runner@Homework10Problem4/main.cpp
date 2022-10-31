79e021ee5ae335b3f9b99f48b599a8fbf3f5d7b4
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-10-31 21:59:38 +0000

    Setting up for loop to compare to total of the while statement

--- .ccls-cache/@home@runner@Homework10Problem4/main.cpp
+++ .ccls-cache/@home@runner@Homework10Problem4/main.cpp
@@ -9,14 +9,23 @@ int main(){
   int nums[NUMS] = {16, 54, 7, 43, -5};  
   
   int total = 0, *nPt;  
+
+  int total2 = 0; // adding int to add up my new for loop
   
   nPt = nums;    
   // store address of nums[0] in nPt 
   
-  while (nPt < nums + NUMS)   
-    total += *nPt++;  
-  
+   while (nPt < nums + NUMS)   
+    total += *nPt++; 
+
   cout << "The total of the array elements is " << total << endl; 
+
+
+    for (int i = 0; i < NUMS; i++){
+      total2 += nums[i];
+  }
+  
+  cout << "The total of the array elements is " << total2 << endl; 
   
   return 0;
   }  
